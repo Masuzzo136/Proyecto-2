e066ee9d01824d03d16921a1df7515066a5ee567
Masuzzo136 <masuzzo136@gmail.com>
2023-06-20 19:45:19 -0300

    proyecto 2

---
+++ main.js
@@ -0,0 +1,17 @@
+const changeFontBtn = document.querySelector('#change-font');
+changeFontBtn.addEventListener('click', function() {
+    document.body.classList.toggle('change-font');
+    
+})
+const enlaceCV = document.getElementById('btn-descargar-cv');
+
+enlaceCV.addEventListener('mouseover', function() {
+  // Aquí puedes agregar el efecto que desees, como cambiar el color del texto, agregar una sombra, etc.
+  enlaceCV.style.color = '#000'; /* Cambia el color del texto al pasar el cursor por encima */
+});
+
+enlaceCV.addEventListener('mouseout', function() {
+  // Aquí puedes revertir el efecto o regresar al estado original
+  enlaceCV.style.color = '#fff'; /* Regresa al color original del texto */
+});
+

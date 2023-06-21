7ebbdb585eacc237eed679971ab0cf8c1f551e5a
Masuzzo136 <masuzzo136@gmail.com>
2023-06-21 12:08:04 -0300

    Proyecto2Final

--- main.js
+++ main.js
@@ -15,3 +15,27 @@ enlaceCV.addEventListener('mouseout', function() {
   enlaceCV.style.color = '#fff'; /* Regresa al color original del texto */
 });
 
+// Obtener referencia al elemento de imagen
+const profileImage = document.getElementById('profile-image');
+
+// Guardar la URL de la primera imagen
+const firstImageURL = 'img/foto1.jpg';
+
+// Guardar la URL de la segunda imagen
+const secondImageURL = 'img/foto2.png';
+
+// Guardar la fuente original de la imagen
+const originalImageSource = profileImage.src;
+
+// Agregar el evento 'mouseenter' al elemento de imagen
+profileImage.addEventListener('mouseenter', function() {
+  // Cambiar la fuente de la imagen al pasar el cursor por encima
+  profileImage.src = secondImageURL;
+});
+
+// Agregar el evento 'mouseleave' al elemento de imagen
+profileImage.addEventListener('mouseleave', function() {
+  // Restaurar la fuente de la imagen cuando el cursor se aleja
+  profileImage.src = originalImageSource;
+});
+
